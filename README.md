55b2aea0c069b18a60aa4440d93ffaad7d472824
Thom Mondeaux <Thommond554@gmail.com>
2020-08-18 12:19:56 -0400

    Initialized repository and edited basic information like lisence and name.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Scientific Self Determination
+
+This is a general website for the Scientific Self Determination and the
+good party. Where all info and related works like podcasts will be available.
+
+
+## Usage
+
+The code of this website can be used as an example of a simple Flask website or
+wiki. You can contribute to this code if errors are in need of fixing. Thanks!
+
+## Contributing
+
+If you would like to see changes to this project skeleton, start a conversation as an Issue to discuss your plans.
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="Scientific-Self-Determination"
+    version="0.0.1",
+    author="Thom Mondeaux, Joe Pekarek",
+    author_email="Thommond@protonmail.com",
+    url="https://github.com/Thommond/Scientific-Self-Determination",
+    description="A website/wiki for SSD and how it could be implemented on political levels.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    extras_require=[],
+    tests_require=['pytest'],
+    python_requires='>=3.6',
+)
